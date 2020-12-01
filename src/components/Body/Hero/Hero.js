59567b3fee3bb14e9029a7685909b7858b2f864b
omarshah0 <oemyoem55@gmail.com>
2020-12-01 18:10:19 +0500

    Initial Commit

---
+++ src/components/Body/Hero/Hero.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import HeroLogo from "../../../assets/Hero-Logo.svg"
+import "./Hero.css"
+function Hero() {
+  const data = useStaticQuery(graphql`
+    query {
+      heroImage: file(relativePath: { eq: "Furniture-Table.png" }) {
+        childImageSharp {
+          fixed(width: 568) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <section id="hero">
+      <div className="hero__container">
+        <div className="hero__description">
+          <p className="hero__description_mainTitle">
+            Are you looking for <strong>wooden furniture</strong> for your
+            place?
+          </p>
+          <span className="hero__description_subTitle">
+            This is the <br />
+            Right Place
+          </span>
+          <button className="hero__description__cta__button">
+            Explore Furniture
+          </button>
+        </div>
+        <div className="hero__image">
+          <Img
+            fixed={data.heroImage.childImageSharp.fixed}
+            alt="Furniture Table"
+          />
+        </div>
+        <div className="hero__background__logo">
+          <HeroLogo />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Header/Header.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { Link } from "gatsby"
+import Logo from "../../assets/Header-Logo.svg"
+import "./Header.css"
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">
+        <div className="header__branding">
+          <div className="header__branding__logo">
+            <Logo />
+          </div>
+          <div className="header__branding__siteTitle">
+            <h1 className="siteTitle">WOODIES</h1>
+          </div>
+        </div>
+      </Link>
+      <div className="header__navigation">
+        <nav>
+          <ul className="header__navigation__list">
+            <li className="header__navigation__list__item">
+              <Link to="/" activeClassName="active">
+                Home
+              </Link>
+            </li>
+            <li className="header__navigation__list__item">
+              <Link to="/about" activeClassName="active">
+                About Us
+              </Link>
+            </li>
+            <li className="header__navigation__list__item">
+              <Link to="/how-it-works" activeClassName="active">
+                How it works
+              </Link>
+            </li>
+            <li className="header__navigation__list__item">
+              <Link to="/categories" activeClassName="active">
+                Categories
+              </Link>
+            </li>
+            <li className="header__navigation__list__item">
+              <Link to="/testimony" activeClassName="active">
+                Testimony
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <button className="header__navigation__cta__button">SIGN UP</button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
